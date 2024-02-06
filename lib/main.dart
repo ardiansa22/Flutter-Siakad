2dcc7cd5cf3d85cd007042c292f40b7545ae27ac
AldiansyahReksa <118238511+AldiansyahReksa@users.noreply.github.com>
2024-02-06 14:59:00 +0700

    rapihin code

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template_tugas_besar/bloc/Khs/khs_bloc.dart';
+import 'package:flutter_template_tugas_besar/data/datasources/auth_local_datasource.dart';
+import 'package:flutter_template_tugas_besar/pages/auth/auth_page.dart';
+import 'package:flutter_template_tugas_besar/pages/mahasiswa/mahasiswa_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: FutureBuilder<bool>(
+        future : AuthLocalDatasource().isLogin(),
+        builder: (context,snapshot){
+          if(snapshot.hasData && snapshot.data!){
+            return BlocProvider(
+              create: (context) => KhsBloc(),
+              child: MahasiswaPage(),
+            );
+          }else {
+            return const AuthPage();
+          }
+        },
+      ),
+    );
+  }
+}
